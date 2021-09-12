9c779fa86dad40a21db3e27457cae5d800078b96
Astha Patidar <patidarastha95@gmail.com>
2021-09-12 23:05:22 +0530

    first commit

---
+++ init.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <dirent.h>
+#include <fstream>
+#include <cstdlib>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int init()
+{
+    if (mkdir(".git") == -1)
+    {
+        cerr << "Error : Directory not created " << strerror(errno) << endl;
+        return 0;
+    }
+    else
+    {
+        if (chdir(".git") == -1)
+        {
+            cerr << "Error :  " << strerror(errno) << endl;
+            return 0;
+        }
+        else
+        {
+            if (mkdir("zero") == -1)
+            {
+                cerr << "Error : Directory not created " << strerror(errno) << endl;
+                return 0;
+            }
+            else
+           {
+                fstream MainIndex;
+                MainIndex.open("Main_Index.txt", ios::out);
+                if (!MainIndex)
+                {
+                    return 0;
+                }
+
+                fstream Gitlog;
+                Gitlog.open("log.txt", ios::out);
+                if (!Gitlog)
+                {
+                    return 0;
+                }
+                if (chdir("zero") == -1)
+                {
+                cerr << "Error : " << strerror(errno) << endl;
+                return 0;
+                }
+                else
+                {
+                  fstream Index;
+                Index.open("index.txt", ios::out);
+                if (!Index)
+                {
+                    return 0;
+                }
+                /*string as = " xcopy" + " C:\\Users\\ps\\Documents\\Gitproject" + " C:\\Users\\ps\\Documents\\Gitproject\\.git\\zero";
+                system(as.c_str());*/
+                }
+
+            }
+        }
+    }
+    return 0;
+}
+
+int main(int argc,char *argv[])
+{
+    string s;
+    s=argv[1];
+    int newrepo;
+    string s1;
+    if(s=="init")
+    {
+        newrepo=init();
+    }
+
+    return 0;
+}
